09ac37c289a8f6d3ac9081943705f3ac39c77758
André Claudino <claudino@d2x.com.br>
2019-06-01 14:31:09 -0300

    Add `setup.py` with server dependencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.1dev',
+    packages=['dino_ia'],
+    requirements=[
+        "flask",
+        "python-socketio"
+    ]
+)
