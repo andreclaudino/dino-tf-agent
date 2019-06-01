244f7856cd1edf1e89fc50ca9e0d018bce01f9c4
André Claudino <claudino@d2x.com.br>
2019-06-01 14:43:45 -0300

    Correct setup.py metadata

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='TowelStuff',
-    version='0.1dev',
+    name='dino_ia',
+    version='0.1',
     packages=['dino_ia'],
+    url='',
+    license='',
+    author='André Claudino',
+    author_email='',
+    description='',
     requirements=[
         "flask",
         "python-socketio"
