38803f86861ca3715b7ebbd825b901e4d3a1acdd
André Claudino <claudino@d2x.com.br>
2019-06-05 07:21:16 -0300

    Move socket configuration to DinoEnv actor

--- dino_ia/server/__init__.py
+++ dino_ia/server/__init__.py
@@ -4,11 +4,23 @@ import eventlet
 from dino_ia.server import socket
 from dino_ia.server import http
 
+http_runner = http.runner()
+socket_runner = socket.socket
 
-def start():
-    http_runner = http.runner()
-    socket_runner = socket.runner()
 
+async def start_server(socket_runner, http_runner):
     app = socketio.WSGIApp(socket_runner, http_runner)
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 3000)), app)
+
+
+# def start():
+#     http_runner = http.runner()
+#     socket_runner = socket.socket
+#
+#     start_server(socket_runner, http_runner)
+#
+#     # while socket.enviroment.locked:
+#     #     print("Wating client connect")
+#
+#     return socket.enviroment
 
--- dino_ia/server/socket.py
+++ dino_ia/server/socket.py
@@ -1,40 +1,8 @@
 import socketio
-from thespian.actors import ActorSystem
-from  dino_ia.actors import PersistenseActor
 
-_sio = socketio.Server()
-_system = ActorSystem()
-_persister = ActorSystem().createActor(PersistenseActor)
+from dino_ia.environment.dino_enviroment import DinoEnv
 
-
-def persist(action, filename, data):
-    message = (action, filename, data)
-    _system.tell(_persister, message)
-
-
-@_sio.on('connect')
-def connect(sid, _):
-    print(f"Connected {sid}")
-    persist('CREATE', f"{sid}.json", None)
-    send("START")
-
-
-@_sio.on('TimeStep')
-def step(sid, data):
-    persist('WRITE', f"{sid}.json", data)
-
-
-@_sio.on('disconnect')
-def disconnect(sid):
-    persist('CLOSE', f"{sid}.json", ())
-    print('disconnect ', sid)
-
-
-def send(action):
-    _sio.emit(action, {})
-
-
-def runner():
-    return _sio
-
+socket = socketio.Server()
+enviroment = DinoEnv()
+socket.register_namespace(enviroment)
 
