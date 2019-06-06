5253241b9b66d1a47a0b0e4f0209794d8d6a5f50
André Claudino <claudino@d2x.com.br>
2019-06-05 23:32:45 -0300

    Remove unuseful code

--- dino_ia/server/http.py
+++ dino_ia/server/http.py
@@ -45,5 +45,4 @@ def runner():
     Start http server
     :return:
     """
-    # app.run(host="0.0.0.0", port="3000")
     return app
--- dino_ia/server/socket.py
+++
@@ -1,8 +0,0 @@
-import socketio
-
-from dino_ia.environment.dino_enviroment import DinoEnv
-
-socket = socketio.Server()
-enviroment = DinoEnv()
-socket.register_namespace(enviroment)
-
