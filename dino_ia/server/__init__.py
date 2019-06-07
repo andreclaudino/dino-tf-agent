8c026d1853856124a3fb604ca528d3c6e53c5add
Claudino <andre.claudino@b2wdigital.com>
2019-06-07 17:49:45 -0300

    Remove eventlet to avoid handshake issues

--- dino_ia/server/__init__.py
+++ dino_ia/server/__init__.py
@@ -1,4 +1,3 @@
-import eventlet
 import socketio
 
 from dino_ia.server import game_socket
@@ -8,19 +7,7 @@ http_runner = http.runner()
 socket_runner = game_socket.socket
 
 
-def start_server(socket_runner, http_runner):
-    app = socketio.WSGIApp(socket_runner, http_runner)
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 3000)), app)
-
-
 def start():
-    http_runner = http.runner()
-    socket_runner = game_socket.socket
-
-    start_server(socket_runner, http_runner)
-
-    # while socket.enviroment.locked:
-    #     print("Wating client connect")
-
-    return game_socket.gameSocket
+    http_runner.wsgi_app = socketio.WSGIApp(socket_runner, http_runner.wsgi_app)
+    http_runner.run(threaded=True, port=3000)
 
--- dino_ia/server/game_socket.py
+++ dino_ia/server/game_socket.py
@@ -2,7 +2,7 @@ import socketio
 
 from dino_ia.server.game_socket_rules import GameSocket
 
-socket = socketio.Server()
+socket = socketio.Server(async_mode='threading')
 gameSocket = GameSocket()
 socket.register_namespace(gameSocket)
 
