7a04278ca5e539c3650590b9123fa72770c78fe9
André Claudino <claudino@d2x.com.br>
2019-06-07 00:49:34 -0300

    Some simple refactors to validate getting state

--- dino_ia/server/game_socket_rules.py
+++ dino_ia/server/game_socket_rules.py
@@ -1,6 +1,7 @@
+from multiprocessing import Queue
+
 import numpy as np
 import socketio
-from multiprocessing import Process, Queue
 
 STATES = {-1: "DOWN", 0: "JUMP", 1: "DUCK"}
 
@@ -8,14 +9,20 @@ STATES = {-1: "DOWN", 0: "JUMP", 1: "DUCK"}
 class GameSocket(socketio.Namespace):
 
     def __init__(self):
+        super().__init__()
+
         self.queue = Queue()
-        self.connected = False
+        self.game_connected = False
         print("Server stated")
 
     def on_connect(self, sid, environ):
-        self.connected = True
-        print("WebPage Conected")
+        self.game_connected = True
+        print("Game Connected")
 
     def on_time_step(self, sid, message):
         state = np.asarray(message['observation']['state'], dtype=np.float32)
         self.queue.put(state)
+        print(message)
+
+    def state(self):
+        return self.queue.get()
