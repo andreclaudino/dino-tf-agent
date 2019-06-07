04242aa7028516b0fbad4ce65bc7a8c74b727d88
Claudino <andre.claudino@b2wdigital.com>
2019-06-07 17:54:39 -0300

    GameSOcket refactoring to add multiprocessing queue support

--- dino_ia/server/game_socket_rules.py
+++ dino_ia/server/game_socket_rules.py
@@ -1,9 +1,6 @@
-from multiprocessing import Queue
-
 import numpy as np
 import socketio
-
-STATES = {-1: "DOWN", 0: "JUMP", 1: "DUCK"}
+from multiprocessing import Queue
 
 
 class GameSocket(socketio.Namespace):
@@ -11,18 +8,24 @@ class GameSocket(socketio.Namespace):
     def __init__(self):
         super().__init__()
 
-        self.queue = Queue()
         self.game_connected = False
+        self._queue = Queue()
+
         print("Server stated")
 
     def on_connect(self, sid, environ):
         self.game_connected = True
         print("Game Connected")
 
-    def on_time_step(self, sid, message):
-        state = np.asarray(message['observation']['state'], dtype=np.float32)
-        self.queue.put(state)
-        print(message)
+    def on_disconnect(self, sid):
+        self.game_connected = False
+        print("Game Disconnected")
 
-    def state(self):
-        return self.queue.get()
+    def on_time_step(self, sid, message):
+        print(message)
+        state = message['state']['observation']
+        self._queue.put(state)
+        print(f"Action {message} returns {state}")
+
+    def get_observable(self):
+        return self._queue.get()
